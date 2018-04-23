f82a855e6574557e6a4342131180192bd5ece118
Gustavo Santanna <gustavo@biexpert.com.br>
2018-04-22 23:39:50 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ProjetoX</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Início do Código PHP</h1>
+<?php
+    // Inicio da aula de PHP
+    /*
+    Comentarios de várias linhas
+    */
+    $nome = "Gustavo";
+    $sobrenome = "Santanna";
+    $idade = 43;
+
+    echo "Meu nome completo é: 
+    ".$nome. " ".$sobrenome. 
+    "e tenho". " ".$idade. 
+    " ". "anos de idade.";
+
+?>
+    
+</body>
+</html>
